905c7e467c5df88f32e1397ff843ccd6b4302fc6
SpaceDadNewman <kendallcwilcox@gmail.com>
2023-10-26 18:11:01 -0600

    init

--- index.mjs
+++ index.mjs
@@ -1,67 +1,70 @@
 import express from 'express';
 import http from 'http';
-import WebSocket from 'ws';
-import {Configuration, OpenAIApi} from 'openai';
+import sharp from 'sharp';
+import axios from 'axios';
+import { OPENAI_API_KEY } from './key.mjs';
 
-const OPENAI_API_KEY = require('./key.mjs');
-console.log(OPENAI_API_KEY);
+const app = express();
+const server = http.createServer(app);
 
+app.use(express.json()); // Enable JSON parsing for requests
 
-// Define your OpenAI instance
-const configuration = new Configuration({
-  organization: "org-lFZ9TR9p1vgOrJHtomepQ0W6",
-  apiKey: OPENAI_API_KEY
-});
+app.post('/analyze', async (req, res) => {
+  const frameData = req.body.frames;
 
-const openai = new OpenAIApi(configuration);
-const response = await openai.listEngines();
+// Process frameData as needed
+// Decode the image
+const image = sharp(frameData);
 
-// Function to list files using OpenAI API
-async function listFiles() {
-  const list = await openai.files.list();
+// Resize the image to a specific width and height
+image.resize(800, 600);
 
-  for await (const file of list) {
-    console.log(file);
+// Convert the image to a different format (e.g., PNG, JPEG)
+const pic = image.toFormat('jpeg');
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, '/picture.jpeg');
+pic.toFile(filePath, function(err) {
+        // error handling
+      });
+// var transformer = sharp()
+//     .rotate()
+//     .resize(500,500)
+//     .toFile(filepath, function(err) {
+//       // error handling
+//     });
+
+filestream.pipe(transformer);
+// Get the processed image as a Buffer
+image.toBuffer((err, processedFrame) => {
+  if (err) {
+    console.error('Error processing frame:', err);
+    return;
   }
-}
 
-// Handle WebSocket connections
-wss.on('connection', (ws) => {
-  console.log('WebSocket connection established');
-
-  // Handle incoming frames from the app
-  ws.on('message', (message) => {
-    const frameData = Buffer.from(message, 'base64');
-    // Processing frame data
-
-    axios.post(apiUrl, { frames: frameData }, {
+  // Processed frame (Buffer) is ready for further use
+});
+  // Make request to OpenAI API
+  try {
+    const response = await axios.post('https://api.openai.com/v1/analyze', {
+      frames: frameData
+    }, {
       headers: {
         'Authorization': `Bearer ${OPENAI_API_KEY}`,
         'Content-Type': 'application/json'
       }
-    })
-    .then((response) => {
-      const analysisResults = response.data;
-      // Handle analysis results
-
-      // WebSocket connection
-      ws.send(JSON.stringify({ type: 'analysis', data: analysisResults }));
-
-      // Call the function to list files using OpenAI API
-      listFiles();
-    })
-    .catch((error) => {
-      console.error(error);
     });
-  });
 
-  // Handle WebSocket disconnections
-  ws.on('close', (code, reason) => {
-    console.log(`WebSocket connection closed. Code: ${code}, Reason: ${reason}`);
-  });
+    const analysisResults = response.data;
+    res.json({ type: 'analysis', data: analysisResults });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
-// Start the server
+const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}/analyze`);
 });
