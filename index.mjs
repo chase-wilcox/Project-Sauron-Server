8999357e9996c09d340c51ef749957baa57ad5b7
SpaceDadNewman <kendallcwilcox@gmail.com>
2023-11-13 11:37:16 -0700

    https

--- index.mjs
+++ index.mjs
@@ -112,6 +112,6 @@ const server = https.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at https://${hostname}:${port}/`);
 });
 
