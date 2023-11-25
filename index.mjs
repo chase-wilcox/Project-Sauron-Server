12d56799cd4d04a2f06304785f91114fa0118e97
SpaceDadNewman <kendallcwilcox@gmail.com>
2023-11-24 23:47:56 -0700

    w

--- index.mjs
+++ index.mjs
@@ -11,38 +11,19 @@ const upload = multer({ storage: storage });
 app.use(express.json());
 
 
-app.get('/videos/:filename', (req, res) => {
-  const videoPath = path.join(__dirname, 'path/to/save', req.params.filename);
+app.get('/', (req, res) => {
+  const videoPath = path.join(__dirname, 'path/to/save', 'your-video-file.mp4');
   const stat = fs.statSync(videoPath);
   const fileSize = stat.size;
-  const range = req.headers.range;
+  const head = {
+    'Content-Length': fileSize,
+    'Content-Type': 'video/mp4',
+  };
 
-  if (range) {
-    const parts = range.replace(/bytes=/, '').split('-');
-    const start = parseInt(parts[0], 10);
-    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
-
-    const chunksize = (end - start) + 1;
-    const file = fs.createReadStream(videoPath, { start, end });
-    const head = {
-      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': chunksize,
-      'Content-Type': 'video/mp4',
-    };
-
-    res.writeHead(206, head);
-    file.pipe(res);
-  } else {
-    const head = {
-      'Content-Length': fileSize,
-      'Content-Type': 'video/mp4',
-    };
-
-    res.writeHead(200, head);
-    fs.createReadStream(videoPath).pipe(res);
-  }
+  res.writeHead(200, head);
+  fs.createReadStream(videoPath).pipe(res);
 });
+
 
 app.post('/uploadVideo', upload.single('video'), (req, res) => {
   // Access the uploaded video file
