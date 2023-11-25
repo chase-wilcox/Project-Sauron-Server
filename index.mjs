f9df8e5767205739cd62632f9c06ee8d98f774bf
SpaceDadNewman <kendallcwilcox@gmail.com>
2023-11-24 23:27:19 -0700

    servver api

--- index.mjs
+++ index.mjs
@@ -1,54 +1,64 @@
-import http from 'http'
-import multer from 'multer'
-import fs from 'fs'
-import express from 'express'
+import express from 'express';
+import multer from 'multer';
+import fs from 'fs';
+import path from 'path';
 
-const hostname = '146.190.175.179';
 const app = express();
 const port = 3000;
 const storage = multer.memoryStorage();  // Change this to store files on disk if needed
 const upload = multer({ storage: storage });
 
-const server = http.createServer((req, res) => {
-  console.log(`Received request for ${req.url}`);
-  if (req.method === 'POST' && req.url === '/uploadVideo') {
-    let data = '';
-    req.on('data', chunk => {
-      data += chunk;
-    });
+app.use(express.json());
 
-    req.on('end', () => {
-      console.log('Received data:', data);
-      // Simulate some asynchronous processing (replace with actual frame processing)
-      setTimeout(() => {
-        const frameData = 'frames'; // Need actual frame data
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ frames: frameData }));
-      }, 2000); // Simulated processing time of 2 seconds
-    });
-  } else {
-    // other requests
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('This server is running\n');
-  }
-  app.post('/uploadVideo', upload.single('video'), (req, res) => {
-    // Access the uploaded video file
-    const videoBuffer = req.file.buffer;
-  
-    // Save the video file to disk or process it as needed
-    const videoPath = 'path/to/save/video.mp4';
-    fs.writeFileSync(videoPath, videoBuffer);
-  
-    // Respond to the client
-    res.status(200).json({ message: 'Video uploaded successfully' });
-  });
-  
-  app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-  });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/videos/:filename', (req, res) => {
+  const videoPath = path.join(__dirname, 'path/to/save', req.params.filename);
+  const stat = fs.statSync(videoPath);
+  const fileSize = stat.size;
+  const range = req.headers.range;
+
+  if (range) {
+    const parts = range.replace(/bytes=/, '').split('-');
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+
+    const chunksize = (end - start) + 1;
+    const file = fs.createReadStream(videoPath, { start, end });
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4',
+    };
+
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    };
+
+    res.writeHead(200, head);
+    fs.createReadStream(videoPath).pipe(res);
+  }
+});
+
+app.post('/uploadVideo', upload.single('video'), (req, res) => {
+  // Access the uploaded video file
+  const videoBuffer = req.file.buffer;
+
+  // Save the video file to disk or process it as needed
+  const videoPath = path.join(__dirname, 'path/to/save', `video_${Date.now()}.mp4`);
+  fs.writeFileSync(videoPath, videoBuffer);
+
+  // Respond to the client
+  res.status(200).json({ message: 'Video uploaded successfully' });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
