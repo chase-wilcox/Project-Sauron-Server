6cbbfca960e63f53b1176baaa426993fb733c121
SpaceDadNewman <kendallcwilcox@gmail.com>
2023-11-24 22:17:27 -0700

    server

--- index.mjs
+++ index.mjs
@@ -6,12 +6,20 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   console.log(`Received request for ${req.url}`);
   if (req.method === 'POST' && req.url === '/uploadVideo') {
-    // Simulate some asynchronous processing (replace with actual frame processing)
-    setTimeout(() => {
-      const frameData = 'frames'; // Need actual frame data
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ frames: frameData }));
-    }, 2000); // Simulated processing time of 2 seconds
+    let data = '';
+    req.on('data', chunk => {
+      data += chunk;
+    });
+
+    req.on('end', () => {
+      console.log('Received data:', data);
+      // Simulate some asynchronous processing (replace with actual frame processing)
+      setTimeout(() => {
+        const frameData = 'frames'; // Need actual frame data
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ frames: frameData }));
+      }, 2000); // Simulated processing time of 2 seconds
+    });
   } else {
     // other requests
     res.statusCode = 200;
