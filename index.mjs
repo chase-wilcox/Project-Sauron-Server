350a0748dcd86c05648082cae1d2139eb5d2f6fe
SpaceDadNewman <kendallcwilcox@gmail.com>
2023-11-24 23:15:00 -0700

    api stuff

--- index.mjs
+++ index.mjs
@@ -1,7 +1,12 @@
 import http from 'http'
+import multer from 'multer'
+import fs from 'fs'
 
 const hostname = '146.190.175.179';
+const app = express();
 const port = 3000;
+const storage = multer.memoryStorage();  // Change this to store files on disk if needed
+const upload = multer({ storage: storage });
 
 const server = http.createServer((req, res) => {
   console.log(`Received request for ${req.url}`);
@@ -26,6 +31,21 @@ const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/plain');
     res.end('This server is running\n');
   }
+  app.post('/uploadVideo', upload.single('video'), (req, res) => {
+    // Access the uploaded video file
+    const videoBuffer = req.file.buffer;
+  
+    // Save the video file to disk or process it as needed
+    const videoPath = 'path/to/save/video.mp4';
+    fs.writeFileSync(videoPath, videoBuffer);
+  
+    // Respond to the client
+    res.status(200).json({ message: 'Video uploaded successfully' });
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
 });
 
 server.listen(port, hostname, () => {
