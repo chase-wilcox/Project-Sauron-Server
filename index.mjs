dccd4230f9217b6e7a3aebfa3d10db6d645a4211
SpaceDadNewman <kendallcwilcox@gmail.com>
2023-11-24 23:34:53 -0700

    changed get

--- index.mjs
+++ index.mjs
@@ -14,38 +14,38 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/videos/:filename', (req, res) => {
-  const videoPath = path.join(__dirname, 'path/to/save', req.params.filename);
-  const stat = fs.statSync(videoPath);
-  const fileSize = stat.size;
-  const range = req.headers.range;
+// app.get('/videos/:filename', (req, res) => {
+//   const videoPath = path.join(__dirname, 'path/to/save', req.params.filename);
+//   const stat = fs.statSync(videoPath);
+//   const fileSize = stat.size;
+//   const range = req.headers.range;
 
-  if (range) {
-    const parts = range.replace(/bytes=/, '').split('-');
-    const start = parseInt(parts[0], 10);
-    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+//   if (range) {
+//     const parts = range.replace(/bytes=/, '').split('-');
+//     const start = parseInt(parts[0], 10);
+//     const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
 
-    const chunksize = (end - start) + 1;
-    const file = fs.createReadStream(videoPath, { start, end });
-    const head = {
-      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': chunksize,
-      'Content-Type': 'video/mp4',
-    };
+//     const chunksize = (end - start) + 1;
+//     const file = fs.createReadStream(videoPath, { start, end });
+//     const head = {
+//       'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+//       'Accept-Ranges': 'bytes',
+//       'Content-Length': chunksize,
+//       'Content-Type': 'video/mp4',
+//     };
 
-    res.writeHead(206, head);
-    file.pipe(res);
-  } else {
-    const head = {
-      'Content-Length': fileSize,
-      'Content-Type': 'video/mp4',
-    };
+//     res.writeHead(206, head);
+//     file.pipe(res);
+//   } else {
+//     const head = {
+//       'Content-Length': fileSize,
+//       'Content-Type': 'video/mp4',
+//     };
 
-    res.writeHead(200, head);
-    fs.createReadStream(videoPath).pipe(res);
-  }
-});
+//     res.writeHead(200, head);
+//     fs.createReadStream(videoPath).pipe(res);
+//   }
+// });
 
 app.post('/uploadVideo', upload.single('video'), (req, res) => {
   // Access the uploaded video file
